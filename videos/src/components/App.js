79f662a35bc09384bfba03b15897ba13aa2e91ce
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2020-10-15 15:19:10 -0400

    added iframe for video

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,13 +4,14 @@ import { Container, Header } from 'semantic-ui-react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 class App extends React.Component {
     constructor(props){
         super(props);
         
-        this.state = { videos:[] }
+        this.state = { videos:[], selectedVideo: null }
     }
 
     onSearchSubmit = async (term) => {
@@ -22,17 +23,22 @@ class App extends React.Component {
         this.setState({videos: response.data.items})
     };
 
+    onVideoSelect = video => {
+        this.setState({ selectedVideo: video })
+    }
+
     render () {
         return (
             <div>
                 <Container>
                 <SearchBar onSearchSubmit={this.onSearchSubmit} />
                 <div className='ui grid'>
-                    <div class='ten wide column'>
+                    <div className='ten wide column'>
                         <h1>Single Video Player</h1>
+                        <VideoDetail video={this.state.selectedVideo} />
                     </div>
-                    <div class='six wide column'>
-                        <VideoList videos={this.state.videos} />
+                    <div className='six wide column'>
+                        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
 
                     </div>
                 </div>
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -20,9 +20,9 @@ import VideoItem from './VideoItem';
 //     };
 // };
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />
+        return <VideoItem video={video} onVideoSelect={onVideoSelect} />
     });
     
     return (
