2483a16d3d849025cd94979f3b584ee9e5cb6a94
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2020-10-17 16:57:35 -0400

    added acordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a faviourite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+]
+
+
 export default () => {
-  return <h1>Accordian App</h1>
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  )
+
 }
