69812f6f261a3121960fe4ceef3031bb2eb5c4a5
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2020-11-02 17:02:46 -0500

    created Reducers

--- ReduxWalkthrough/reduxExercise.js
+++ ReduxWalkthrough/reduxExercise.js
@@ -34,4 +34,33 @@ const createClaim = (name, amountOfMoney) => {
       }
     };
   };
-  
+
+// Reducers (Departments)
+const claimsHistory = (oldList = [], action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        // ... is creating a new array, whereas .push() is modifying an existing one
+        return [...oldList, action.payload];
+    }
+
+    return oldList;
+}
+
+const accounting = (currentMoney = 100, action) => {
+    if (action.type === 'CREATE_CLAIM'){
+        return currentMoney - action.payload.amountOfMoney;
+    } else if (action.type === 'CREATE_POLICY'){
+        return currentMoney + action.payload.amountOfMoney;
+    }
+
+    return currentMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+        return [...listOfPolicies, action.payload.name];
+    } else if (action.type === 'DELETE_POLICY') {
+        return listOfPolicies.filter(name => name!==action.payload.name);
+    }
+
+    return listOfPolicies;
+};
