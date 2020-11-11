681f51688c9b3f13cc75b5fb399beb9f5e51b499
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2020-11-11 08:43:03 -0500

    hooked up middleware react-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,29 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// export const fetchPosts = async () => {
+//     const response = await jsonPlaceholder.get('/posts');
+//     return {
+//         type: 'FETCH_POSTS',
+//         payload: response
+//     };
+// };
+
+// Redux thunk allows actions to return a function!
+// export const fetchPosts = () => {
+//     return async function(dispatch, getState) {
+//         const response = await jsonPlaceholder.get('/posts');
+
+//         dispatch({
+//             type: 'FETCH_POST',
+//             payload: response 
+//         });
+//     };
+
+// };
+
+// Refactored
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POST', payload: response });
 };
