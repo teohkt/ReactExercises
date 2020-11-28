288b82eb06aabc6681092c255bb1053051030d3b
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2020-11-27 22:18:01 -0500

    added reducers, connected streamsList to redux store

--- streams/client/src/components/SubmitForm.js
+++ streams/client/src/components/SubmitForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { ErrorMessage } from '@hookform/error-message';
 import { connect, useDispatch } from 'react-redux';
 import { createStream } from '../actions';
 
@@ -12,37 +13,39 @@ function SubmitForm() {
     function onSubmit(formValues) {
         console.log(formValues);
         dispatch(createStream(formValues));
+        // createStream(formValues);
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)} className="ui form">
                 <label>Title</label> 
-                {errors.title && errors.title.type === "required" && (
+                {/* {errors.title && errors.title.type === "required" && (
                     <span role="alert">: This field is required</span>
-                )}
+                )} */}
                 <input 
                     name="title"
                     ref={register({
-                        required: true
+                        required: "This is required."
                     })} 
                     placeholder="Title" 
                     style={{borderColor: errors.title && "red"}}
                 />
+                <ErrorMessage errors={errors} name="title" as="p" message="This is required"/>
 
                 <label>Description</label>
-                {errors.title && errors.title.type === "required" && (
+                {/* {errors.title && errors.title.type === "required" && (
                     <span role="alert">: This field is required</span>
-                )}
+                )} */}
                 <input 
                     name="description"
                     ref={register({
-                        required: true
+                        required: "This is required"
                     })}
                     placeholder="Description" 
                     style={{borderColor: errors.description && "red"}}
                 />
-
+                <ErrorMessage errors={errors} name="description" as="p"/>
 
                 <button type="submit">Submit</button>
             </form>
--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -2,8 +2,11 @@ import { combineReducers } from 'redux';
 // import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer';
+import formReducer from '../components/SubmitForm';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     auth: authReducer,
-    // form: formReducer
+    // form: formReducer,
+    streams: streamReducer
 });
