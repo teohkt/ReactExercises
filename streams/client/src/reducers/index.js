e17973cf6533cb04f6f2ec6418d50492d426478b
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2020-11-30 21:09:11 -0500

    added modal

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
-// import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer';
-import formReducer from '../components/SubmitForm';
+// import formReducer from '../components/SubmitForm';
 import streamReducer from './streamReducer';
+
 
 export default combineReducers({
     auth: authReducer,
--- streams/client/src/streams/StreamDelete.js
+++ streams/client/src/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Modal from '../components/Modal';
 
 const StreamDelete = () => {
-    return <div>StreamDelete</div>
-}
+    return (
+        <div>
+            <h1>StreamDelete</h1>
+            <Modal />
+        </div>
+    );
+}; 
 
 export default StreamDelete;
