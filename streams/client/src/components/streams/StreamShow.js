648605f60bd67bd494b0513744111f377d9dc056
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2020-12-02 21:42:07 -0500

    connected obs stream

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -11,7 +11,31 @@ class StreamShow extends React.Component {
     }
 
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const { id } = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render(){
@@ -22,7 +46,7 @@ class StreamShow extends React.Component {
         const {title, description} = this.props.stream;
         return (
             <div>
-                <video ref={this.videoRef} />
+                <video ref={this.videoRef} style={{ width: '100%' }} controls={true}/>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
